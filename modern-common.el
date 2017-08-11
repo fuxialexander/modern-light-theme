70e4d113bf3eb1c8a9ac0ee48c16fc7e8e196602
fuxialexander <fuxialexander@gmail.com>
2017-08-12 00:45:32 +0800

    vp

--- modern-common.el
+++ modern-common.el
@@ -142,7 +142,7 @@
             `(doom-modeline-bar                      ((,class (:background ,blue))))
             ;; `(doom-modeline-eldoc-bar             ((,class ())))
             `(doom-modeline-inactive-bar             ((,class (:background ,blue-bg))))
-            `(window-divider ((,class (:foreground act1))))
+            `(window-divider ((,class (:foreground ,act1))))
 
 
 ;;;;; git-gutter+
