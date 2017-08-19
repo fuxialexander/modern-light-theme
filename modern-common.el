903baf9596bd3ca72e801a5faf276e01063534a6
fuxialexander <fuxialexander@gmail.com>
2017-08-19 23:22:33 +0800

    Fix: lazy-highlight

--- modern-common.el
+++ modern-common.el
@@ -267,7 +267,7 @@
             `(highlight                              ((,class (:foreground ,base :background ,highlight))))
             `(hl-line                                ((,class (:background ,bg2))))
             `(isearch                                ((,class (:foreground ,bg1 :background ,mat))))
-            `(lazy-highlight                         ((,class (:background ,green-bg-s :weight normal))))
+            `(lazy-highlight                         ((,class (:background ,blue-bg :weight normal))))
             `(link                                   ((,class (:foreground ,comment :underline t))))
             `(link-visited                           ((,class (:foreground ,comp :underline t))))
             `(match                                  ((,class (:background ,highlight :foreground ,mat))))
